bc3de3e00e5abaa12355a3ae570b6d62c8ce7d29
倪勇勤 <527678809@qq.com>
2020-11-10 17:17:56 +0800

    update: 规范命名,优化方法

--- timekit/timekit.go
+++ timekit/timekit.go
@@ -17,26 +17,19 @@ func NowInSecs() int64 {
 	return time.Now().Unix()
 }
 
+
 // UTCNowTime returns current time in utc.
 func UTCNowTime() time.Time {
 	return time.Now().UTC()
 }
 
-/*
-从 octopus/async/utils/time_util.go 中迁移过来的函数
- */
-// CurrentTimeStamp returns current time in utc.
-func CurrentTimeStamp() int64 {
-	return NowInSecs()
-}
-
-// TimeStamp2UTCTime timestamp int64 in seconds to utc time
-func TimeStamp2UTCTime(t int64) time.Time {
+// SecondToUTCTime returns second to time
+func SecondToUTCTime(t int64) time.Time{
 	return time.Unix(t, 0).UTC()
 }
 
-// UTCTime2Timestamp  time.Time to timestamp in seconds
-func UTCTime2Timestamp(t time.Time) int64 {
+// SecondToUTCTime returns utc time to second
+func UTCTimeToSecond(t time.Time) int64 {
 	if t.IsZero() {
 		return 0
 	} else {
@@ -44,19 +37,9 @@ func UTCTime2Timestamp(t time.Time) int64 {
 	}
 }
 
-// UnixMSec time.Time 转为 毫秒
-func UnixMSec(t time.Time) int64 {
-	return t.UnixNano() / 1e6
-}
-
-// CurrentUTCTime returns utc time.Time
-func CurrentUTCTime() time.Time {
-	return UTCNowTime()
-}
-
-// ZeroTime returns 当前北京时间的当天的0点0时0分的 utc的时间
-func ZeroTime() time.Time {
-	now := CurrentUTCTime()
+// TodayZeroUTCTime return 当前北京时间的当天的0点0时0分的 utc的时间
+func TodayZeroUTCTime() time.Time {
+	now := UTCNowTime()
 	secondsEastOfUTC := int((8 * time.Hour).Seconds())
 	bjZone := time.FixedZone("Beijing", secondsEastOfUTC)
 	bjTime := now.In(bjZone)
@@ -65,33 +48,34 @@ func ZeroTime() time.Time {
 	return zeroBjTime.In(time.UTC)
 }
 
-// e.g. 20170109031201 2017-01-09 03:12:01
-func CurrentUTCTimeStrInSecs() string {
-	return CurrentUTCTime().Format("20060102150405")
+// NowToYMDhms returns current time which is str. eg: 20060102150405
+func NowToYMDhms() string{
+	return UTCNowTime().Format("20060102150405")
 }
 
-// e.g. 20170109 2017-01-09
-func CurrentUTCDateStr() string {
-	return CurrentUTCTime().Format("20060102")
+// NowToYMD returns current time which is str. eg: 20060102
+func NowToYMD() string{
+	return UTCNowTime().Format("20060102")
 }
 
-// e.g. 2017-01-09 03:12:01
-func CurrentUTCDateDetailStr() string {
-	return CurrentUTCTime().Format("2006-01-02 15:04:05")
+// NowToStandardDate e.g. 2017-01-09 03:12:01
+func NowToStandardDate() string{
+	return UTCNowTime().Format("2006-01-02 15:04:05")
 }
 
-// e.g. 2017-01-09 03:12:01
-func Int642CurrentUTCDateDetailStr(t int64) string {
-	tmpTime := time.Unix(t, 0)
+// SecondToStandardDate  e.g.  1604996388 to  2017-01-09 03:12:01
+func SecondToStandardDate(s int64) string{
+	tmpTime := time.Unix(s, 0)
 	return tmpTime.UTC().Format("2006-01-02 15:04:05")
 }
 
-func DescBetweenTime(t1, t2 time.Time) float64 {
+// Diff return t1 - t2
+func Diff(t1, t2 time.Time) float64 {
 	return t1.Sub(t2).Seconds()
 }
 
 // e.g. 2020-11-10 00:00:00 +0800 CST
-func Timestamp2DateTime(t int64) time.Time {
+func SecondToZeroLocTime(t int64) time.Time {
 	tmpTime := time.Unix(t, 0)
 	year, month, day := tmpTime.Date()
 	dateTime := time.Date(year, month, day, 0, 0, 0, 0, tmpTime.Location())
@@ -104,35 +88,25 @@ func MaxTime() time.Time {
 	return t
 }
 
-// CheckTimeBetween returns targetTime is between startTime and endTime
-func CheckTimeBetween(targetTime, startTime, endTime time.Time) bool {
-	targetStamp := UTCTime2Timestamp(targetTime)
-	startStamp := UTCTime2Timestamp(startTime)
-	endStamp := UTCTime2Timestamp(endTime)
+// IsBetween judges targetTime is between startTime and endTime
+func IsBetween(targetTime, startTime, endTime time.Time) bool {
+	targetStamp := UTCTimeToSecond(targetTime)
+	startStamp := UTCTimeToSecond(startTime)
+	endStamp := UTCTimeToSecond(endTime)
 	return targetStamp <= endStamp && targetStamp >= startStamp
 }
 
-// AsyncCountCostms 从 start开始算过了多少秒
-func AsyncCountCostms(start time.Time) int64 {
-	return time.Since(start).Nanoseconds() / 1e6
+// MillsSinceTime returns millSecond since start time
+func MillsSinceTime(start time.Time) int64 {
+	return time.Since(start).Nanoseconds() / int64(time.Millisecond)
 }
 
-// BeforeMonthUtcDateSepStr
-func BeforeMonthUtcDateSepStr(t time.Time, monthNum int) string {
-	return t.AddDate(0, -monthNum, 0).Format("2006-01-02")
+// AddDateToStr
+func AddDateToStr(t time.Time, year,month,day int) string{
+	return t.AddDate(year, month, day).Format("2006-01-02")
 }
 
-// AfterDayUtcDateSepStr
-func AfterDayUtcDateSepStr(t time.Time, dayNum int) string {
-	return t.AddDate(0, 0, dayNum).Format("2006-01-02")
-}
-
-// BeforeDayUtcDate
-func BeforeDayUtcDate(t time.Time, dayNum int) time.Time {
-	return t.AddDate(0, 0, -dayNum)
-}
-
-// AfterDayUtcDate
-func AfterDayUtcDate(t time.Time, dayNum int) time.Time {
-	return t.AddDate(0, 0, dayNum)
+// SubDateToStr
+func SubDateToStr(t time.Time, year,month,day int) string{
+	return t.AddDate(-year, -month, -day).Format("2006-01-02")
 }
